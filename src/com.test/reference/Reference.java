c31837bedd881a83e140a8e256999af8aa185b1d
O-harmash <blind_a_med@bbq.agency>
2022-08-22 20:43:22 +0300

    add BaseTest

---
+++ src/com.test/reference/Reference.java
@@ -0,0 +1,13 @@
+package src.com.test.reference;
+
+public class Reference {
+
+    public static void main(String[] args) {
+        Person olga = new Person();
+
+
+        Person inna = new Person();
+
+
+    }
+}
---
+++ src/com.test/reference/primitive.java
@@ -0,0 +1,20 @@
+package src.com.test.reference;
+
+public class primitive {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        System.out.println("a= "+ a +", b = " + b);
+
+        swap(a, b);
+
+        System.out.println("a= "+ a +", b = " + b);
+    }
+
+    private static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
